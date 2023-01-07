1a63c3b379557d77b3a8b5b3a33096a68d9a6232
shivasaicharanruthala <ruthala.s@norhteastern.edu>
2023-01-07 10:23:31 -0500

    day-2

---
+++ L511.sql
@@ -0,0 +1,17 @@
+-- Game Play Analysis I
+
+IF EXISTS (SELECT 1 FROM INFORMATION_SCHEMA.TABLES WHERE TABLE_TYPE='BASE TABLE' AND TABLE_NAME='Activity')
+    DROP table Activity;
+
+Create table Activity (player_id int, device_id int, event_date date, games_played int);
+
+insert into Activity (player_id, device_id, event_date, games_played) values ('1', '2', '2016-03-01', '5');
+insert into Activity (player_id, device_id, event_date, games_played) values ('1', '2', '2016-05-02', '6');
+insert into Activity (player_id, device_id, event_date, games_played) values ('2', '3', '2017-06-25', '1');
+insert into Activity (player_id, device_id, event_date, games_played) values ('3', '1', '2016-03-02', '0');
+insert into Activity (player_id, device_id, event_date, games_played) values ('3', '4', '2018-07-03', '5');
+
+
+-- Query
+select player_id, min(event_date) as first_login from Activity group by player_id order by player_id;
+
